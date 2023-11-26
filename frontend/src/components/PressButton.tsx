d87da59bb603e55fd290033f2683b3312a0898d9
Nimpoo <marwan.ayoub.pro@gmail.com>
2023-11-26 18:47:16 +0100

    `components`: better `UX` for `PressButton` (and One Piece forever)

--- frontend/src/components/PressButton.tsx
+++ frontend/src/components/PressButton.tsx
@@ -21,20 +21,20 @@ export default function PressButton( {color, children} : {color: string, childre
 
 			{/* ANGLE_1 */}
 			<div className={`absolute 
-			-z-10 -bottom-2 left-2 w-2 h-[calc(1.41421356237%+0.67rem)]
+			-z-10 -bottom-2 left-2 w-4 h-[calc(1.41421356237%+0.67rem)]
 			bg-${color}-500 border-2 border-${color}-900
 			origin-bottom-left -rotate-45 transition-all
-			${isHovered ? 'group-hover:h-[calc(1.41421356237%+0.30rem)] group-active:w-0 group-active:opacity-0' : ''}
+			${isHovered ? 'group-hover:h-[calc(1.41421356237%+1.02rem)] group-active:h-[calc(0.33rem)] duration-75' : ''}
 			`}
 			>
 			</div>
 
 			{/* ANGLE_2 */}
 			<div className={`absolute
-			-z-10 top-2 -right-2 w-[calc(1.41421356237%+0.62rem)] h-2
+			-z-10 top-2 -right-2 w-[calc(1.41421356237%+0.57rem)] h-4
 			bg-${color}-500 border-2 border-${color}-900
 			origin-top-right rotate-45 transition-all
-			${isHovered ? 'group-hover:w-[calc(1.41421356237%+0.25rem)] group-active:h-0 group-active:opacity-0' : ''}
+			${isHovered ? 'group-hover:w-[calc(1.41421356237%+0.94rem)] group-active:w-[calc(0.33rem)] duration-75' : ''}
 			`}
 			>
 			</div>
@@ -44,7 +44,7 @@ export default function PressButton( {color, children} : {color: string, childre
 			bg-gray-100
 			border-2 border-black
 			w-36 h-16 transition transform
-			hover:translate-y-1 hover:translate-x-1 active:translate-y-2 active:translate-x-2
+			hover:-translate-y-1 hover:-translate-x-1 active:translate-y-[calc(0.25rem)] active:translate-x-[calc(0.25rem)]
 			hover:bg-${color}-300 active:bg-${color}-400`}
 				onMouseEnter={() => setIsHovered(true)}
 				onMouseLeave={() => setIsHovered(false)}
