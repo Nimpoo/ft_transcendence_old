6f1ec3ed25a1e8f39ea2090440b24f0c39231690
Nimpoo <marwan.ayoub.pro@gmail.com>
2023-12-21 23:41:32 +0100

    `/page.tsx`: header + title screen done (with animation)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,27 @@
 "use strict"
 "use client"
 
-import PressButton from "@/components/PressButton"
+import '@/styles/HomeTitle.css'
 
 export default function Home(): JSX.Element {
 
 	return (
-		<div className="min-h-screen flex items-center justify-center">
-			{/* <LoginButton /> */}
-			<PressButton color="red">
-				Home
-			</PressButton>
+
+		<div className="ml-20 flex items-start justify-start mt-24">
+			<h1 id="title" className="
+			xl:text-9xl lg:text-8xl md:text-7xl sm:text-5xl
+			text-gray-900 dark:text-white text-start
+			">
+				<span>
+					Welcome<br />
+				</span>
+				<span>
+					to our<br />
+				</span>
+				<span>
+					Final Project.
+				</span>
+			</h1>
 		</div>
 	)
 }
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,53 @@
+"use strict"
+"use client"
+
+export default function Header(): JSX.Element {
+
+	return (
+
+		<header className="
+				bg-neutral-950 bg-opacity-[0.08]
+				mt-12 ml-20 mr-20
+				xl:h-20 lg:h-16 
+				flex justify-center items-center
+				">
+
+			<button>
+				<img className="
+					h-4 xl:h-20 lg:h-16 md:h-10 sm:h-6
+					mr-3 drop-shadow-[4px_4px_2px_rgba(0,0,0,0.25)]" src="/mugiwara/ussop.png" alt="Sniperking !"
+				/>
+			</button>
+
+			<button>
+				<img className="
+					h-4 xl:h-20 lg:h-16 md:h-10 sm:h-6
+					mr-3 drop-shadow-[4px_4px_2px_rgba(0,0,0,0.25)]" src="/mugiwara/nami.png" alt="Tangarine !"
+				/>
+			</button>
+			<p className="
+				text-gray-900 dark:text-white
+				xl:text-6xl lg:text-5xl md:text-3xl sm:text-2xl
+				text-center drop-shadow-[4px_3px_1px_rgba(0,0,0,0.25)]
+			">
+
+				The Transcendence
+
+			</p>
+			<button>
+				<img className="
+					h-3 xl:h-[4.25rem] lg:h-[3.25rem] md:h-[2.25rem] sm:h-[1.5rem]
+					ml-3 drop-shadow-[4px_4px_2px_rgba(0,0,0,0.25)]" src="/mugiwara/zoro.png" alt="Saber !"
+				/>
+			</button>
+
+			<button>
+				<img className="
+					h-3 xl:h-[4.25rem] lg:h-[3.25rem] md:h-[2.25rem] sm:h-[1.5rem]
+					ml-3 drop-shadow-[4px_4px_2px_rgba(0,0,0,0.25)]" src="/mugiwara/luffy.png" alt="Meat !" />
+			</button>
+
+		</header>
+
+	)
+}
