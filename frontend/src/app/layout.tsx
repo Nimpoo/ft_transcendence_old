b23ed79d95c221f0bcf9b520b32658449b6c1c24
Nimpoo <marwan.ayoub.pro@gmail.com>
2023-12-21 23:40:09 +0100

    `/layout.tsx`: background done

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,17 @@
-'use strict'
+"use strict"
 
+import Header from "@/components/Header"
 import { Metadata } from "next"
 import "tailwindcss/tailwind.css"
+
+import '@/styles/Bg.scss'
+
+import { Ubuntu } from 'next/font/google'
+
+const ubu = Ubuntu ({
+	subsets: ['latin'],
+	weight: '500'
+})
 
 export const metadata: Metadata = {
 	title: 'ft_transcendence',
@@ -11,7 +21,19 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body className="bg-[url('/background.gif')] bg-cover bg-fixed bg-center bg-no-repeat backdrop-brightness-50 w-full h-screen">
+			<head>
+			</head>
+			<body className={ "bg-[url('/bg.png')] bg-cover bg-fixed bg-center bg-no-repeat " + ubu.className }>
+				<Header />
+				<div className="gradient-bg">
+					<div className="gradient-container">
+						<div className="g1"></div>
+						<div className="g2"></div>
+						<div className="g3"></div>
+						<div className="g4"></div>
+						<div className="g5"></div>
+					</div>
+				</div>
 				{children}
 			</body>
 		</html>
